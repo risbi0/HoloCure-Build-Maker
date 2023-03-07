b0008d3078cb76962498705938ad1f17b39744f9
teduniq <reyestedowen123@gmail.com>
2023-03-07 22:00:57 +0800

    add Vercel Analytics

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,11 @@
 	import Link from '$lib/Link.svelte';
 	import SaveImage from '$lib/SaveImage.svelte';
     import Choices from '$lib/Choices/Choices.svelte';
+
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <main id="main-container">
